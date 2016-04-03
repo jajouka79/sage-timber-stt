40f21c1290ba6f80d21e2540584c99c09cdc5a66
Simon <sbeasley@leicestercollege.ac.uk>
2016-04-03 01:37:21 +0100

    testing patronbase

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<?php
+home.php<?php
 
 $context = Timber::get_context();
 
@@ -20,6 +20,24 @@ if( ! empty ( $query ) ) :
 endif;
 
 // echo "test";
-echo "test2";
+// echo "test2";
+
+
+#curl https://uk.patronbase.com/_LeicesterYMCA/API/v1/Productions/Feed \-H "X-PatronBase-Api-Key:ba28aed6329c27827b63b3043419752f45f2f438"
+
+
+// set post fields
+$post = [
+    'X-PatronBase-Api-Key' => 'ba28aed6329c27827b63b3043419752f45f2f438',
+];
+
+$ch = curl_init('https://uk.patronbase.com/_LeicesterYMCA/API/v1/Productions/Feed');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+$response = curl_exec($ch);
+curl_close($ch);
+var_dump($response);
+
+
 
 Timber::render('templates/home.twig', $context);
